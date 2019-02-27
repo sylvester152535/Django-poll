ae003f239bef873e8bf812e443c704a8b181c174
ShaunHoward <sylvester152535@yahoo.com>
2019-02-26 21:50:15 -0500

    Pushing Django Polls App Tutorial

---
+++ mysite/polls/models.py
@@ -0,0 +1,27 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date < now
+
+
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice_text
